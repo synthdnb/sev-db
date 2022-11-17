5d0db4ac8e42dc3fb047becb585c98ca46b7c7ce
Marshall Kim <synthdnb@gmail.com>
2022-11-17 10:26:21 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'import',
+    'react',
+    '@typescript-eslint',
+  ],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'webpack.config.js',
+  ],
+  rules: {
+    'arrow-body-style': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', {
+      code: 120,
+      tabWidth: 2,
+      ignoreComments: true,
+    }],
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+    }],
+  },
+};
